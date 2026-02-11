2cd6e201f05b5150b0dcb5a3761cff71ae47d6ee
gehud <grozdovnikolay@gmail.com>
2026-02-11 16:15:44 +0300

    fix: fixing dataset file reading

--- src/dataset.hpp
+++ src/dataset.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <charconv>
 #include <cstring>
 #include <expected>
 #include <filesystem>
@@ -21,16 +22,16 @@ namespace stocc {
 
 class dataset_error : public std::runtime_error {
 public:
-    dataset_error(const std::string& what)
-        : std::runtime_error(std::format("Dataset error: {}", what)) {}
+    dataset_error(const std::string& what) :
+        std::runtime_error(std::format("Dataset error: {}", what)) {}
 
     dataset_error(
         const fs::path& path,
         int line,
         int column,
         const std::string& what
-    )
-        : std::runtime_error(std::format(
+    ) :
+        std::runtime_error(std::format(
             "Dataset error (at {}:{}:{}): {}",
             path.string(),
             line,
@@ -54,9 +55,9 @@ public:
     dataset(const dataset& other) = delete;
 
     static std::expected<dataset, dataset_error> load(const fs::path& path) {
-        std::ifstream stream(path);
+        std::ifstream file(path);
 
-        if (!stream.is_open()) {
+        if (!file.is_open()) {
             return std::unexpected(dataset_error(std::format(
                 "failed to open file ('{}'): {}",
                 path.string(),
@@ -65,37 +66,44 @@ public:
         }
 
         std::string line;
-        std::getline(stream, line);
+        std::getline(file, line);
 
-        std::stringstream line_stream(line);
+        std::stringstream row(line);
         std::string cell;
 
-        auto receive_ts_index = std::numeric_limits<size_t>::max();
-        auto price_index = std::numeric_limits<size_t>::max();
+        auto receive_ts_field = std::numeric_limits<size_t>::max();
+        auto price_field = std::numeric_limits<size_t>::max();
 
-        size_t index = 0;
-        while (std::getline(line_stream, cell, ';')) {
+        size_t field = 0;
+        while (std::getline(row, cell, ';')) {
             if (cell == "receive_ts") {
-                receive_ts_index = index;
+                receive_ts_field = field;
             } else if (cell == "price") {
-                price_index = index;
+                price_field = field;
             }
 
-            ++index;
+            ++field;
         }
 
-        if (receive_ts_index == std::numeric_limits<size_t>::max()) {
-            return std::unexpected(dataset_error(path, 1, 1, "missing 'receive_ts' field"));
+        if (receive_ts_field == std::numeric_limits<size_t>::max()) {
+            return std::unexpected(dataset_error(
+                path,
+                1,
+                1,
+                "missing 'receive_ts' field"
+            ));
         }
 
-        if (price_index == std::numeric_limits<size_t>::max()) {
-            return std::unexpected(dataset_error(path, 1, 1, "missing 'price' field"));
+        if (price_field == std::numeric_limits<size_t>::max()) {
+            return std::unexpected(dataset_error(
+                path,
+                1,
+                1,
+                "missing 'price' field"
+            ));
         }
 
-        dataset dataset;
-        dataset._receive_ts_index = receive_ts_index;
-        dataset._price_index = price_index;
-        dataset._stream = std::move(stream);
+        dataset dataset(path, std::move(file), receive_ts_field, price_field);
 
         auto next_result = dataset.next();
 
@@ -107,58 +115,123 @@ public:
     }
 
     std::expected<void, dataset_error> next() {
-        if (!_stream) {
+        if (!_file) {
             return {};
         }
 
         auto read_line_result = read_line();
 
         if (!read_line_result.has_value()) {
-            _stream.reset();
+            _file.reset();
             return std::unexpected(read_line_result.error());
         }
 
         if (!read_line_result.value()) {
-            _stream.reset();
+            _file.reset();
         }
 
         return {};
     }
 
     std::optional<dataset_record> current() const {
-        return _stream ? std::optional(_record) : std::nullopt;
+        return _file ? std::optional(_record) : std::nullopt;
     }
 private:
-    size_t _receive_ts_index;
-    size_t _price_index;
-    std::optional<std::ifstream> _stream;
+    fs::path _path;
+    std::optional<std::ifstream> _file;
+    size_t _receive_ts_field;
+    size_t _price_field;
     dataset_record _record;
+    size_t _line;
 
-    dataset() = default;
+    dataset(
+        const fs::path& path,
+        std::ifstream&& file,
+        size_t receive_ts_index,
+        size_t price_index
+    ) :
+        _path(path),
+        _receive_ts_field(receive_ts_index),
+        _price_field(price_index),
+        _file(std::move(file)),
+        _line(1)
+    {}
 
     std::expected<bool, dataset_error> read_line() {
-        if (!_stream) {
+        std::string line;
+        if (!std::getline(*_file, line)) {
             return false;
         }
 
-        std::string line;
-        std::getline(*_stream, line);
+        ++_line;
 
-        std::stringstream line_stream(line);
+        std::stringstream row(line);
         std::string cell;
 
-        size_t index = 0;
-        while (std::getline(line_stream, cell, ';')) {
-            if (index == _receive_ts_index) {
-                _record.receive_ts = std::stoul(cell);
-            } else if (index == _price_index) {
-                _record.price = std::stod(cell);
+        auto receive_ts_read = false;
+        auto price_read = false;
+        int column = 1;
+        size_t field = 0;
+        while (std::getline(row, cell, ';')) {
+            if (field == _receive_ts_field) {
+                auto result = std::from_chars(
+                    cell.data(),
+                    cell.data() + cell.size(),
+                    _record.receive_ts
+                );
+
+                if (result.ec != std::errc()) {
+                    return std::unexpected(dataset_error(
+                        _path,
+                        _line,
+                        column,
+                        "could not convert 'receive_ts' cell"
+                    ));
+                }
+
+                receive_ts_read = true;
+            } else if (field == _price_field) {
+                auto result = std::from_chars(
+                    cell.data(),
+                    cell.data() + cell.size(),
+                    _record.price
+                );
+
+                if (result.ec != std::errc()) {
+                    return std::unexpected(dataset_error(
+                        _path,
+                        _line,
+                        column,
+                        "could not convert 'price' cell"
+                    ));
+                }
+
+                price_read = true;
             }
 
-            ++index;
+            column += cell.size() + 1;
+            ++field;
         }
 
-        return !_stream->eof();
+        if (!receive_ts_read) {
+            return std::unexpected(dataset_error(
+                _path,
+                _line,
+                column,
+                "missing 'receive_ts' cell"
+            ));
+        }
+
+        if (!price_read) {
+            return std::unexpected(dataset_error(
+                _path,
+                _line,
+                column,
+                "missing 'price' cell"
+            ));
+        }
+
+        return true;
     }
 };
 
@@ -175,7 +248,11 @@ private:
 
     struct data {
         std::vector<dataset> files;
-        std::priority_queue<entry, std::vector<entry>, std::greater<entry>> queue;
+        std::priority_queue<
+            entry,
+            std::vector<entry>,
+            std::greater<entry>
+        > queue;
     };
 public:
     class iterator {
@@ -193,13 +270,17 @@ public:
         }
 
         iterator& operator++() {
+            if (!_current.has_value()) {
+                _data = nullptr;
+                return *this;
+            }
+
             _data->queue.pop();
 
             auto& file = _data->files[_current.value().stream_id];
             auto next_result = file.next();
 
             if (!next_result.has_value()) {
-                _data = nullptr;
                 _current = std::unexpected(next_result.error());
                 return *this;
             }
